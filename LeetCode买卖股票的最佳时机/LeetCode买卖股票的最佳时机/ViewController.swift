b9402964bd20bbbe330e547327238140a4973645
YYJ <842901013@qq.com>
2018-07-19 16:34:35 +0800

    LeetCode

---
+++ LeetCode买卖股票的最佳时机/LeetCode买卖股票的最佳时机/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  LeetCode买卖股票的最佳时机
+//
+//  Created by yyj on 2018/7/18.
+//  Copyright © 2018年 yyj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        maxProfit([4,4,6,1,1,4,2,5])
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //利用差价计算盈亏
+    
+    func maxProfit(_ prices: [Int]) -> Int {
+        
+        if prices.count < 2 {
+            return 0
+        }
+        
+#if false    //第一题
+        
+//        guard prices.count > 0 else {
+//            return 0
+//        }
+//        var local = 0
+//        var global = 0
+//        for i in 1..<prices.count {
+//            local = max(local+prices[i]-prices[i-1], 0)
+//            global = max(global, local)
+//        }
+//        return global
+        
+        var new:[Int] = []
+        for index in 0...prices.count-2 {
+            
+            new.append(prices[index+1] - prices[index])
+        }
+        
+        var temp = 0
+        var maxPrice = 0
+        
+        for price in new {
+            
+            if price + temp > 0 {
+                
+                temp += price
+            } else {
+                
+                temp = 0
+            }
+            
+            maxPrice = max(maxPrice, temp)
+        }
+        
+        return maxPrice
+#endif
+
+
+#if true      //第二题
+        
+        var maxPrice = 0
+        
+        for index in 0...prices.count-2 {
+            
+            let different = prices[index+1] - prices[index]
+            
+            if different > 0 {
+                
+                maxPrice += different
+            }
+        
+        }
+        
+        return maxPrice
+        
+#endif
+        
+#if false      //第三题
+        
+        
+        
+#endif
+        
+    }
+
+
+}
+
--- LeetCode寻找旋转排序数组中的最小值 II/LeetCode寻找旋转排序数组中的最小值 II/ViewController.swift
+++ LeetCode寻找旋转排序数组中的最小值 II/LeetCode寻找旋转排序数组中的最小值 II/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print(findMin([5,5,5,1,2,3,4,5]))
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,31 +25,36 @@ class ViewController: UIViewController {
     }
 
     
-    
+    //首先还是二分,但是判断条件需要修改,因为存在重复元素,所以当mid>=left和mid<=right时无法判断最小值在哪个区间
+    //只有当mid>right能够确定最小值∈[mid, right],mid<left时最小值∈[left, mid]
+    //此时只能右移一位再次判断,不能采用二分
     func findMin(_ nums: [Int]) -> Int {
         
         if nums.count == 0 {return 0}
         if nums.count == 1 {return nums[0]}
         
         var left = 0
-        var right = nums.count
-        
-        if nums[left] < nums[right] {
-            return nums[left]
-        }
+        var right = nums.count-1
         
         
         while left < right {
             
+            if nums[left] < nums[right] {
+                return nums[left]
+            }
+            
             let mid = (left + right)/2
             
-            if nums[mid] >= nums[right] {
+            if nums[mid] > nums[right] {
                 
                 left = mid + 1
                 
             } else if nums[mid] < nums[left] {
                 
                 right = mid
+            } else {
+                
+                left += 1
             }
             
         }
